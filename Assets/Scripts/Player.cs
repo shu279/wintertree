df548fd95866d4e85a4b0000f733fcbfba5494c6
shu279 <shusuke279@gmail.com>
2022-01-29 15:04:26 +1100

    プレイヤーの移動処理を追加

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
     }
+
+    public void Move()
+    {
+        float currentSpeed = 0;
+        if (Input.GetKey(KeyCode.W))
+        {
+            currentSpeed = speed;
+        }
+        float angle = transform.eulerAngles.y;
+        float x = currentSpeed * Mathf.Sin(angle * Mathf.PI / 180f);
+        float z = currentSpeed * Mathf.Cos(angle * Mathf.PI / 180f);
+        rb.velocity = new Vector3(x, rb.velocity.y, z);
+    }
+    [SerializeField] float speed;
 }
