5ea8970bfbd2814aa445cfa04550c5cf9847a86c
shu279 <shusuke279@gmail.com>
2022-02-05 16:35:04 +1100

    カメラの追従スクリプトを修正

--- Assets/Scripts/AimObject.cs
+++ Assets/Scripts/AimObject.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class AimObject : MonoBehaviour
 {
-    private Vector2 rotation;
+    public Vector2 rotation;
     [SerializeField] Vector2 rotationSpeed;
     [SerializeField] Transform playerCenterPosition;
+    [SerializeField] float maxRotationX;
 
     //半径
     private float r = 5;
@@ -23,20 +24,29 @@ public class AimObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow) && rotation.x<maxRotationX)
         {
             rotation += new Vector2(rotationSpeed.x, 0);
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow) && rotation.x>maxRotationX*-1)
         {
             rotation -= new Vector2(rotationSpeed.x, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rotation += new Vector2(0, rotationSpeed.y);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rotation -= new Vector2(0, rotationSpeed.y);
         }
 
         //位置確定
         Vector3 diff = new Vector3(
-            0,
+            r * Mathf.Sin(rotation.y * Mathf.PI / 180f),
             r * Mathf.Sin(rotation.x * Mathf.PI / 180f),
-            r * Mathf.Cos(rotation.x * Mathf.PI / 180f));
+            r * Mathf.Cos(rotation.x * Mathf.PI / 180f)+ r* Mathf.Cos(rotation.y * Mathf.PI / 180f)
+            );
         transform.position = playerCenterPosition.position + diff;
     }
 }
--- Assets/Scripts/CameraAim.cs
+++ Assets/Scripts/CameraAim.cs
@@ -7,15 +7,17 @@ public class CameraAim : MonoBehaviour
     public GameObject player,aimObject;
     private Vector3 offset;
     private Vector3 setPosition;
+    [SerializeField] Transform playerCenterPosition;
 
     //半径
-    private float r = 5;
+    private float r;
     //ラジアン
     private float deg = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        r = Mathf.Sqrt((playerCenterPosition.position.x * playerCenterPosition.position.x) +
+            (playerCenterPosition.position.z * playerCenterPosition.position.z));
     }
 
     // Update is called once per frame
@@ -27,5 +29,10 @@ public class CameraAim : MonoBehaviour
         transform.position = setPosition;
         transform.LookAt(aimObject.transform);*/
         transform.LookAt(aimObject.transform);
+        float rotationY = aimObject.GetComponent<AimObject>().rotation.y;
+        Vector3 diff = new Vector3(
+           r * Mathf.Sin(rotationY * Mathf.PI / 180f+Mathf.PI), transform.position.y-playerCenterPosition.position.y, r * Mathf.Cos(rotationY * Mathf.PI / 180f + Mathf.PI));
+        transform.position = playerCenterPosition.position + diff;
+    
     }
 }
